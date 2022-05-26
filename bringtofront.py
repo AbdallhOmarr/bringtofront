662441db095679764076662bfb7e0ca9d635d85f
Abdallh Omarr <AbdallhOmarr@gmail.com>
2022-05-26 13:27:56 +0200

    a

--- bringtofront.py
+++ bringtofront.py
@@ -10,7 +10,13 @@ def bring_window_to_front(window_title):
     top_windows = []
     win32gui.EnumWindows(windowEnumerationHandler, top_windows)
     for i in top_windows:
-        if window_title in i[1].lower():
+        # print(i)
+        print(i[1].lower())
+        if window_title.lower() in i[1].lower():
+            print("found window")
             win32gui.ShowWindow(i[0], 5)
             win32gui.SetForegroundWindow(i[0])
             break
+
+
+bring_window_to_front('Document1 - Word')
